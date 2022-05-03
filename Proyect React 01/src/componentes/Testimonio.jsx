72ea8acdff61678a24c65dc2ca297be0d0c416da
Dilmer-R <rojascastaneda893@gmail.com>
2022-05-03 09:06:40 -0500

    r

---
+++ Proyect React 01/src/componentes/Testimonio.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../css/Testimonio.css';
+
+function Testimonio(props) {
+	return (
+		<div className='contenedor-testimonio'>
+			{ <img src={require(`../imagenes/testimonio-${props.imagen}.png`)} alt="images" className='imagen-testimonio'/>}
+			<div className='contenedor-texto-testimonio'>
+				<p className='nombre-testimonio'><strong>{props.nombre} </strong>en {props.pais}</p>
+				<p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong></p>
+				<p className="texto-testimonio">"{props.testimonio}"</p>
+			</div>
+		</div>
+	);
+}
+// Exportacion por defecto:
+
+export default Testimonio;
